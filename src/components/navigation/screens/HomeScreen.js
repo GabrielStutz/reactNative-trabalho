9f1c1b3f221ebd36a99ed874968b4079901a6db1
PbSanches <pblegalll@gmail.com>
2023-09-04 20:53:21 -0300

    Home?

--- src/components/navigation/screens/HomeScreen.js
+++ src/components/navigation/screens/HomeScreen.js
@@ -1,23 +1,83 @@
-import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { View, Text, FlatList, Image } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function HomeScreen() {
-    return (
-        <View style={styles.View}>
-            <Text onPress={() => alert("This is the home screen.")} style={styles.Text}>Home Screen</Text>
-        </View>
-    );
+
+const Tab = createBottomTabNavigator();
+
+
+const HomeScreen = () => {
+  const [locations, setLocations] = React.useState([
+    {
+      name: 'McWay Falls',
+      image: require('./mcway-falls.jpg'),
+    },
+    {
+      name: 'Garrapata Beach',
+      image: require('./garrapata-beach.jpg'),
+    },
+    {
+      name: 'Julia Pfeiffer Beach',
+      image: require('./julia-pfeiffer-beach.jpg'),
+    },
+  ]);
+
+
+  return (
+    <View>
+      <Text>Minhas doações</Text>
+      <FlatList
+        data={locations}
+        renderItem={({ item }) => (
+          <View>
+            <Image source={item.image} />
+            <Text>{item.name}</Text>
+          </View>
+        )}
+        keyExtractor={(item) => item.name}
+      />
+    </View>
+  );
 };
 
-const styles = StyleSheet.create({
-    View: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    Text: {
-        fontSize: 26,
-        fontWeight: "bold",
-    },
-});
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+
+const SavedScreen = () => {
+  return (
+    <View>
+      <Text>Saved Screen</Text>
+    </View>
+  );
+};
+
+
+const FilterScreen = () => {
+  return (
+    <View>
+      <Text>Filter Screen</Text>
+    </View>
+  );
+};
+
+
+const App = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Saved" component={SavedScreen} />
+      <Tab.Screen name="Filter" component={FilterScreen} />
+    </Tab.Navigator>
+  );
+};
+
+
+export default App;
